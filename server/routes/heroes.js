dc47f8165d8bbeff822cbb07547fdc5e637a5ea4
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-28 15:28:10 -0600

    really needed more time

--- server/routes/heroes.js
+++ server/routes/heroes.js
@@ -40,60 +40,60 @@ router.get('/', function (req, res) {
 //     });
 // });
 
-router.post('/', function (req, res) {
-  var newHero = req.body;
-  console.log('New Hero: ', newHero);
-  pool.connect()
-    .then(function (client) {
-      client.query('INSERT INTO heroes (persona, alias, power_id) VALUES ($1, $2, $3)',
-        [newHero.persona, newHero.alias, newHero.power_id])
-        .then(function (result) {
-          client.release();
-          res.sendStatus(201);
-        })
-        .catch(function (err) {
-          console.log('error on INSERT', err);
-          res.sendStatus(500);
-        });
-    });
-});
-//Request to delete Hero from DB
-router.delete('/:id', function(req, res) {
-  var heroId = req.params.id;
-  console.log('Deleting hero ID:, ', heroId);
-  pool.connect()
-    .then(function (client) {
-      client.query('DELETE FROM heroes WHERE id = $1',
-        [heroId])
-        .then(function (result) {
-          client.release();
-          res.sendStatus(200);
-        })
-        .catch(function (err) {
-          console.log('error on SELECT', err);
-          res.sendStatus(500);
-        });
-    });
-});
-
-router.put('/:id', function(req, res) {
-  var heroId = req.params.id;
-  var hero = req.body;
-  console.log('Updating hero:, ', hero);
-  pool.connect()
-    .then(function (client) {
-      client.query('UPDATE heroes SET persona = $1, alias = $2, power_id = $3 WHERE id = $4',
-        [hero.persona, hero.alias, hero.power_id, hero.id])
-        .then(function (result) {
-          client.release();
-          res.sendStatus(200);
-        })
-        .catch(function (err) {
-          console.log('error on UPDATE', err);
-          res.sendStatus(500);
-        });
-    });
-});
-
-
-module.exports = router;
+// router.post('/', function (req, res) {
+//   var newHero = req.body;
+//   console.log('New Hero: ', newHero);
+//   pool.connect()
+//     .then(function (client) {
+//       client.query('INSERT INTO heroes (persona, alias, power_id) VALUES ($1, $2, $3)',
+//         [newHero.persona, newHero.alias, newHero.power_id])
+//         .then(function (result) {
+//           client.release();
+//           res.sendStatus(201);
+//         })
+//         .catch(function (err) {
+//           console.log('error on INSERT', err);
+//           res.sendStatus(500);
+//         });
+//     });
+// });
+// //Request to delete Hero from DB
+// router.delete('/:id', function(req, res) {
+//   var heroId = req.params.id;
+//   console.log('Deleting hero ID:, ', heroId);
+//   pool.connect()
+//     .then(function (client) {
+//       client.query('DELETE FROM heroes WHERE id = $1',
+//         [heroId])
+//         .then(function (result) {
+//           client.release();
+//           res.sendStatus(200);
+//         })
+//         .catch(function (err) {
+//           console.log('error on SELECT', err);
+//           res.sendStatus(500);
+//         });
+//     });
+// });
+//
+// router.put('/:id', function(req, res) {
+//   var heroId = req.params.id;
+//   var hero = req.body;
+//   console.log('Updating hero:, ', hero);
+//   pool.connect()
+//     .then(function (client) {
+//       client.query('UPDATE heroes SET persona = $1, alias = $2, power_id = $3 WHERE id = $4',
+//         [hero.persona, hero.alias, hero.power_id, hero.id])
+//         .then(function (result) {
+//           client.release();
+//           res.sendStatus(200);
+//         })
+//         .catch(function (err) {
+//           console.log('error on UPDATE', err);
+//           res.sendStatus(500);
+//         });
+//     });
+// });
+//
+//
+// module.exports = router;
